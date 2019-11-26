13638acc2cc880e905035e1cdef624b733e8c636
Saurabh Gupta <saurabh3949@gmail.com>
2019-11-26 15:25:56 -0800

    Remove unrequired files

--- reinforcement_learning/bandits_recsys_movielens/recobanditsgym/agents/__init__.py
+++
@@ -1,5 +0,0 @@
-from recobanditsgym.agents.reco_bandit_agent import RecoBanditAgent
-from recobanditsgym.agents.lin_ucb import LinUCBAgent
-from recobanditsgym.agents.vw_agent import VWAgent
-from recobanditsgym.agents.lin_ts import LinTSAgent
-from recobanditsgym.agents.random_agent import RandomAgent
--- reinforcement_learning/bandits_recsys_movielens/recobanditsgym/utils.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from sklearn.preprocessing import normalize
-
-
-def uniform_feature_vec(dim: int):
-    vec = np.random.rand(1, dim)
-    vec = normalize(vec, norm='l2', axis=1)
-    return vec
